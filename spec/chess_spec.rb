6a7460eae6f3d5768b7a83a76a5f5e79f01e3b75
Scott <scottpmccann@gmail.com>
2022-07-26 15:14:54 -0700

    remove unuse file

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -3,9 +3,4 @@
 require_relative '../lib/moves'
 
 describe Moves do
-  subject(:game) { described_class.new }
-
-  describe '#knight_check' do
-    it 'returns true if the knight puts the king in check'
-  end
 end
